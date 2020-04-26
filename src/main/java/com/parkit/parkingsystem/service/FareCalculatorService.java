874786f4ee9f7a45588180f7d8f43ca56f5b28cd
Loïc Stringer <loic.stringer@gmail.com>
2020-04-26 12:33:13 +0200

    Modify the calculate fare method in order to display minutes

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -8,30 +8,30 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-	public void calculateFare(Ticket ticket) {
-		if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
-			throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
-		}
+	public void calculateFare(Ticket ticket){
+        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
+            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
+        }
 
-		int inHour = ticket.getInTime().getHours();
-		int outHour = ticket.getOutTime().getHours();
+   		Instant instantIn = ticket.getInTime().toInstant();
+   		Instant instantOut = ticket.getOutTime().toInstant();
+       
 
-		// TODO: Some tests are failing here. Need to check if this logic is correct
-		int duration = outHour - inHour;
+        
+   		long duration = Duration.between(instantIn, instantOut).toMinutes();
 
-		switch (ticket.getParkingSpot().getParkingType()) {
-		case CAR: {
-			ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
-			break;
-		}
-		case BIKE: {
-			ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
-			break;
-		}
-		default:
-			throw new IllegalArgumentException("Unkown Parking Type");
-		}
-	}
+        switch (ticket.getParkingSpot().getParkingType()){
+            case CAR: {
+                ticket.setPrice(duration * (Fare.CAR_RATE_PER_HOUR/60));
+                break;
+            }
+            case BIKE: {
+                ticket.setPrice(duration * (Fare.BIKE_RATE_PER_HOUR/60));
+                break;
+            }
+            default: throw new IllegalArgumentException("Unknown Parking Type");
+        }
+    }
 
 	public void calculateRegularUsersReducedFare(Ticket ticket) {
 		if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
